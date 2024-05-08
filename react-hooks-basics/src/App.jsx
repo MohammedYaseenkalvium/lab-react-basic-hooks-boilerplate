db237a0808cd0947a88bd5b5b006aaf3c74a40fe
Mohammed Yaseen <mohammed.yaseen@kalvium.community>
2024-05-08 12:23:19 +0530

    react-basic-hooks

---
+++ react-hooks-basics/src/App.jsx
@@ -0,0 +1,16 @@
+import { useContext, useState } from "react";
+import "./App.css";
+import Content from "./components/Content";
+import Context from "./components/Context";
+
+function App() {
+  return (
+    <div>
+      <Context>
+        <Content />
+      </Context>
+    </div>
+  );
+}
+
+export default App;
